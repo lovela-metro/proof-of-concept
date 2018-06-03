c09584f3e665302ff47772dccca084c72fee53df
Jen Diamond <thejendiamond@gmail.com>
2018-06-03 15:02:59 -0700

    Saturday June 2nd Proof of Concept - LoveLA

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div ref="vgmap">
+    <gmap-map class="google-map"
+      :center="center"
+      :zoom="inzoom">
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="false"
+      ></gmap-marker>
+    </gmap-map>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    mounted: function() {
+      this.geolocation()
+    },
+    data () {
+      return {
+        inzoom : 15,
+        center: {lat: 10.0, lng: 10.0},
+        markers: [],
+/*        markersx: [
+         {position: {lat: 10.0, lng: 10.0}},
+         {position: {lat: 11.0, lng: 11.0}}
+        ]*/
+      }
+    },
+    methods: {
+      geolocation() {
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          }
+          var myPosition = {position: { lat: position.coords.latitude,
+                              lng: position.coords.longitude,}};
+          this.markers.push(myPosition);
+          console.log(myPosition);
+          console.log(this.markers);
+        });
+
+     }
+    }
   }
-}
 </script>
 
 <style>
@@ -25,4 +65,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+  .google-map {
+    width: 700px;
+    height: 400px;
+    margin: 0 auto;
+    background: gray;
+  }
+
+  .button {
+     display: inline-block;
+     margin: -4px;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyC13JVX4Fa8C2mqaI32QhxqSIONruM0z9A',
+    libraries: 'places',
+  }
+})
 
 new Vue({
   render: h => h(App)
